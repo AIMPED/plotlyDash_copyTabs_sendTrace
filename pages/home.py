93c418cedd3fa912c14351b385476b5655f5185f
AIMPED <79043640+AIMPED@users.noreply.github.com>
2023-11-22 10:45:11 +0000

    added comments

--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,12 @@
-from dash import register_page, html, callback, Input, Output
+from dash import register_page, html
+
 
 # register page in the registry
 register_page(__name__, path="/")
 
+# layout of the page (landing page)
 layout = html.Div(
     [
-        html.Div(id='container_page_1', children='landing page')
+        html.Div(id='container_page_1', children='This is just a dummy page')
     ]
 )
